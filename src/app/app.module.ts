1264bed5a937da16769e694dd008606c140fe181
Dritero <driteroavd@gmail.com>
2018-12-28 22:41:33 +0100

    Add ngx charts example in milestones page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { MilestonesComponent } from './milestones/milestones.component';
 import { BalanceModelComponent } from './balance-model/balance-model.component';
 import { WeeksInLifeComponent } from './weeks-in-life/weeks-in-life.component';
+import {NgxChartsModule} from "@swimlane/ngx-charts";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import { WeeksInLifeComponent } from './weeks-in-life/weeks-in-life.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
